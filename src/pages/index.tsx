7656e9a9008c402a0cbe81a14fed000b26725f3e
azukhrufy <anandazukhrufawalwi@gmail.com>
2023-03-08 02:45:56 +0700

    :sparkles: FE: implement list of repo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,20 @@ import TopHeader from "@/components/Navbar/TopHeader";
 import GithubIcon from "../../public/icons/github.svg";
 import { useSelector } from "react-redux";
 import { RootState } from "@/state/store";
-import { Pagination } from "antd";
+import { Pagination, Button } from "antd";
+import List from "@/components/List/List";
 
 export default function Home() {
   const githubService = new GithubService();
   const [user, setUser] = useState<any>();
-  const [repoData, setRepoData ] = useState<any>([]);
+  const [repoData, setRepoData] = useState<any>([]);
   const [totalPage, setTotalPage] = useState<number>(0);
   const [minIndex, setMinIndex] = useState(0);
   const [maxIndex, setMaxIndex] = useState(0);
   const [current, setCurrent] = useState(1);
-  const searchUser = useSelector((state: RootState) => state.searchUser.searchUserData);
+  const searchUser = useSelector(
+    (state: RootState) => state.searchUser.searchUserData
+  );
 
   useEffect(() => {
     async function fetchUserData() {
@@ -24,8 +27,7 @@ export default function Home() {
       const repo = (await githubService.getRepo(searchUser)).data;
       setUser(data);
       setRepoData(repo);
-      setTotalPage(repo.length/5);
-      setMaxIndex(5);
+      setMaxIndex(4);
     }
 
     fetchUserData();
@@ -33,10 +35,10 @@ export default function Home() {
 
   const handleChangePage = (page: any) => {
     setCurrent(page);
-    setMinIndex((page - 1) * 5);
-    setMaxIndex(page * 5);
-  }
-  if (!user) return <p>Loading</p>;
+    setMinIndex((page - 1) * 4);
+    setMaxIndex(page * 4);
+  };
+  if (!user || !repoData) return <p>Loading</p>;
   return (
     <>
       <Head>
@@ -57,24 +59,41 @@ export default function Home() {
         <div className="content">
           <div className="profile-container">
             <div className="image-container">
-              <img src={user?.avatar_url} />
+              <img src={user.avatar_url} />
             </div>
             <div className="text-container">
-              <h1>{user?.name}</h1>
-              <h3>{user?.login}</h3>
-              <p>{user?.bio}</p>
+              <h1>{user.name}</h1>
+              <h3>{user.login}</h3>
+              <p>{user.bio}</p>
+              <Button href={user.html_url} className="mt-4 max-w-4xl">
+                See Profile on Github
+              </Button>
             </div>
           </div>
+
           <div className="repo-container">
-            {repoData && repoData.map((r : any, i : any) => i >= minIndex && i <= maxIndex && (
-              <p key={i}>{r.name}</p>
-            ))}
-            <Pagination
-              pageSize={5}
-              current={current}
-              total={repoData.length}
-              onChange={(page) => handleChangePage(page)}
-            />
+            {repoData &&
+              repoData.map(
+                (r: any, i: any) =>
+                  i >= minIndex &&
+                  i <= maxIndex && (
+                    <a href={r.html_url}>
+                      <List key={i}>
+                        <h1>{r.name}</h1>
+                        <p className="lang">{r.language}</p>
+                        <p>{r.description}</p>
+                      </List>
+                    </a>
+                  )
+              )}
+            {repoData && repoData.length > 0 && (
+              <Pagination
+                pageSize={4}
+                current={current}
+                total={repoData.length}
+                onChange={(page) => handleChangePage(page)}
+              />
+            )}
           </div>
         </div>
       </main>
